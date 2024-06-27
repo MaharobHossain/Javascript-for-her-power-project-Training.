eda48a78f586d52780eed6db207aaccac92e11a6
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2024-06-27 17:34:55 +0600

    boolean type variable

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
-var myVillage ='9482388'
+var myVillage ='jfsfbff'
 console.log(typeof myVillage);
 
 
 var mobNum = 01788899999;
 console.log(typeof mobNum)
+
+var boolType = true ;
+console.log(boolType)
+
+var moonType = false;
+console.log(typeof moonType)
